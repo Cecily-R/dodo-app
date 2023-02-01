aa8176ad1c4bf2adf813ccc05f09e83f9b379e2f
Cecily Rowland <cec1rowland@gmail.com>
2023-02-01 15:58:00 +0000

    Breaks map into continents and adds on click handler

--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { ComposableMap, Geographies, Geography, Annotation, ZoomableGroup} from "react-simple-maps";
+import { ComposableMap, Geographies, Geography, Annotation, ZoomableGroup, Marker} from "react-simple-maps";
 import { geoPatterson } from "d3-geo-projection";
 import Navbar from './navBar.js';
 
 
-const geoURL = "https://raw.githubusercontent.com/lotusms/world-map-data/main/world.json";
+const geoURL = "https://raw.githubusercontent.com/deldersveld/topojson/master/world-continents.json";
 
 const width = 800;
 const height = 500;
@@ -13,23 +13,8 @@ const projection = geoPatterson()
   .translate([width / 2, height / 1.75])
   .scale(100);
 
-
 const Map = ({setTooltipContent}) => {
   const [countries, setCountries] =  useState([])
-
-  // function handleZoomIn() {
-  //   // if (position.zoom >= 4) return;
-  //   setPosition((pos) => ({ ...pos, zoom: pos.zoom * 2 }));
-  // }
-
-  // function handleZoomOut() {
-  //   if (position.zoom <= 1) return;
-  //   setPosition((pos) => ({ ...pos, zoom: pos.zoom / 2 }));
-  // }
-
-  // function handleMoveEnd(position) {
-  //   setPosition(position);
-  // }
 
   const getData = () => {
     fetch('http://localhost:3001/countries', {
@@ -46,10 +31,14 @@ const Map = ({setTooltipContent}) => {
     getData()
   }, [])
 
+  const handleClick = (geo) => () => {
+    console.log(geo);
+  };
+
   return (
     <div className="Map">
         <ComposableMap width={width} height={height} projection={projection} position="relative">
-            <ZoomableGroup center={[0, 0]} zoom={1}>
+            <ZoomableGroup center={[0, 0]} zoom={1.25}>
               <Geographies geography={geoURL}>
                 {({geographies}) =>
                   geographies.map((geo, index) => {
@@ -57,38 +46,25 @@ const Map = ({setTooltipContent}) => {
                       <Geography
                       key={index}
                       geography={geo}
-                      onMouseEnter={() => {                  
-                        const NAME  = geo.properties.name;
-                        console.log(`${NAME}`)
-                        setTooltipContent(`${NAME}`);
-                        <p id="my-element" data-tooltip-content="hello world">
-                          Tooltip
-                        </p>
-                      }}
-                      onMouseLeave={() => {
-                        setTooltipContent("");
-                      }}
+                      onClick={handleClick(geo.properties)}
                       style={{
-                        
                         hover: {
                           fill: "#32ff83",
                           outline: "none"
                         },
                         pressed: {
-                          fill: "#d2c3ff",
+                          fill: "#32ff83",
                           outline: "none"
                         },
                         default: {
                             outline: 'none'
                         }
-                        
                       }}
                       />
                     )
                   })
                 }
-              </Geographies>  
-
+              </Geographies> 
             </ZoomableGroup>
         </ComposableMap>
       </div>
