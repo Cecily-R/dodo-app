06e496714519ecbbc93db8647ab0de4248c917c2
Cecily Rowland <cec1rowland@gmail.com>
2023-02-01 11:51:40 +0000

    Remove git issues

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
 import Map from './Map.js';
 import Navbar from './navBar.js';
 import React, {useState} from 'react';
 import { Tooltip as ReactTooltip } from "react-tooltip";
 import 'react-tooltip/dist/react-tooltip.css' //needed?
-=======
-import React, { useState, useEffect } from "react";
->>>>>>> b2cc1d1 (Added JSON db containing list of countries and fetched in App.js file)
 
 function App() {
   const [content, setContent] = useState("");
 
-<<<<<<< HEAD
   return (
     <div>
       <div className="navbar">
@@ -22,29 +17,6 @@ function App() {
       {/*<p id="my-element" data-tooltip-content={content}>Tooltip</p>*/}
           <Map setTooltipContent={setContent}/>
       </div>
-=======
-const App = () => {
-  const [countries, setCountries] =  useState([])
-
-  const getData = () => {
-    fetch('http://localhost:3001/countries', {
-      header:{
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      }
-    })
-    .then((res) => res.json())
-    .then((data) => {setCountries(data)})
-  }
-
-  useEffect(() => {
-    getData()
-  }, [])
-
-  return (
-    <div className="App">
-      
->>>>>>> b2cc1d1 (Added JSON db containing list of countries and fetched in App.js file)
     </div>
   );
 };
