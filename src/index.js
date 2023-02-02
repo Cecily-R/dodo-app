6865feafaf111cc0691b18d8918956b69732cd1c
Anthony Shaw <ashaw0192@gmail.com>
2023-02-02 10:39:04 +0000

    old code new styling

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Navbar from './navBar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+      <App className="App"/>
   </React.StrictMode>
 );
 
