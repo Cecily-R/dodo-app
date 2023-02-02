87799c37b940e861fffce1806cbf1664b37f4a2f
Cecily Rowland <cec1rowland@gmail.com>
2023-02-02 12:05:42 +0000

    Add first test

--- api/tests/RedListClient.test.js
+++ api/tests/RedListClient.test.js
@@ -2,7 +2,7 @@ require("jest-fetch-mock").enableMocks();
 const RedListClient = require("./lib/RedListClient")
 
 describe("RedListClient", () => {
-  describe("fetchEndangeredSpecies", () => {
+  describe("fetchSpeciesByCountry", () => {
     beforeEach(() => {
       fetch.resetMocks();
     });
@@ -21,6 +21,13 @@ describe("RedListClient", () => {
           "Redpoll"
         ],
       }));
+
+      client.fetchSpeciesByCountry("Azerbaijan", (data) => {
+        expect(data.name).toEqual("Azerbaijan")
+        expect(data.assoc_animals[0]).toEqual("Northern Goshawk")
+        expect(data.assoc_animals[5]).toEqual("Redpoll")
+        expect(data.assoc_animals.length).toEqual(6)
+      })
     });
   });
 });
