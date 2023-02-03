2d925519cfb976086fa5c11ac9c60361024cddf4
Cecily Rowland <cec1rowland@gmail.com>
2023-02-03 10:06:44 +0000

    Added bottom bar

--- src/bottomBar.js
+++ src/bottomBar.js
@@ -2,7 +2,7 @@ const BottomBar = () => {
   return (
     <header>
       <div className="bottomBar">
-        click on a continent to see the species that need our help the most<br />
+        click on a continent to see the species in the animal kingdom that need our help the most<br />
         built by Anthony, Cecily, JC, Mike and Naila
       </div>
     </header>
