635f932e3473e7319e841f28a464fe032a369cae
Cecily Rowland <cec1rowland@gmail.com>
2023-03-08 11:53:29 +0000

    Fixed Breaking News button

--- src/News.js
+++ src/News.js
@@ -7,7 +7,7 @@ const News = () => {
   const date = new Date().toISOString().split('T')[0]
   const apiKey = 'da86dadbff91415699979cdc79e21197';
 
-  const newsApiURL = 'https://newsapi.org/v2/everything?q=animal conservation&from=' + `${date}` + '&sortBy=popularity&apiKey=' + `${apiKey}`
+  const newsApiURL = 'https://newsapi.org/v2/everything?q=animal conservation&from' + `${date}` + '&sortBy=popularity&apiKey=' + `${apiKey}`
   useEffect(() => {
     fetchNews()
   }, []);
